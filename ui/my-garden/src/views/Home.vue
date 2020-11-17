0d8c0e38fd3477d50d4122bb460cf2537fe841eb
Dan Mathews <daniel.mathews@hotmail.co.uk>
2020-11-17 00:00:30 +0000

    vue working in ui container

---
+++ ui/my-garden/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <navbar></navbar>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import navbar from '@/components/navbar.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    navbar,
+    HelloWorld
+  }
+}
+</script>
