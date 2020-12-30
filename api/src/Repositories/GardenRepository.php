058cd483d1a8ed9fcfd3b371bb71c689d45a2dff
Dan Mathews <daniel.mathews@hotmail.co.uk>
2020-12-30 01:56:34 -0800

    removed unnecessary log

--- api/src/Repositories/GardenRepository.php
+++ api/src/Repositories/GardenRepository.php
@@ -116,8 +116,6 @@ class GardenRepository extends Repository
            'dimension_y' => $garden->getDimensionY(),
         ]);
 
-        error_log($garden->getId());
-
         /** @noinspection PhpNonStrictObjectEqualityInspection this is on purpose */
         if(
             $stmt->rowCount() < 1 &&
