1d86d43cf1764df5a7bb0eb7b594450f2a703dce
Dan Mathews <daniel.mathews@hotmail.co.uk>
2021-01-13 12:40:29 +0000

    CR tweaks

--- api/src/App.php
+++ api/src/App.php
@@ -5,7 +5,6 @@ declare(strict_types = 1);
 namespace MyGarden;
 
 use MyGarden\Controllers\ControllerCollection;
-use MyGarden\Database\DatabaseConnection;
 use MyGarden\Factories\SimpleControllerCollectionFactory;
 use MyGarden\Request\Request;
 
